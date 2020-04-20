b03132bd869ee9745e34e151c2b9358ea4189c52
Vishal <varhatia@gmail.com>
2020-04-20 17:24:09 +0530

    added travis file

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi There Champ!!!
+          Hi The Champ!!!
         </p>
         <a
           className="App-link"
